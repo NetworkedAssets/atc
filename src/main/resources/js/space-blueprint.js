49893ecb35e4708df2a4b21d6fb43fb4a85baf36
Tina Steiger <tsteiger@networkedassets.org>
2015-07-09 15:46:38 +0200

    Added more pages and made some configurations

--- src/main/resources/js/space-blueprint.js
+++ src/main/resources/js/space-blueprint.js
@@ -1,13 +1,16 @@
+/*Hook into the wizard apis. Update the space home page title to be the space name
+  and a suffix of " Home Page".*/
+
 AJS.bind("blueprint.wizard-register.ready", function () {
     function submitExampleSpace(e, state) {
-        state.pageData.ContentPageTitle = state.pageData.name + " " + AJS.I18n.getText("confluence.blueprints.space.example.home.title.suffix"); 
+        state.pageData.ContentPageTitle = state.pageData.name + " Home Page"; 
         return Confluence.SpaceBlueprint.CommonWizardBindings.submit(e, state);
     }
     function preRenderExampleSpace(e, state) {
         state.soyRenderContext['atlToken'] = AJS.Meta.get('atl-token');
         state.soyRenderContext['showSpacePermission'] = false; 
     }
-    Confluence.Blueprint.setWizard('com.example.plugins.tutorial.confluence.spacebp.spacebp:arc-space-blueprint-item', function(wizard) {
+   Confluence.Blueprint.setWizard('com.networkedassets.plugins.space-blueprint:space-blueprint-item', function(wizard) {
         wizard.on("submit.exampleSpaceId", submitExampleSpace);
         wizard.on("pre-render.exampleSpaceId", preRenderExampleSpace);
         wizard.on("post-render.exampleSpaceId", Confluence.SpaceBlueprint.CommonWizardBindings.postRender);
