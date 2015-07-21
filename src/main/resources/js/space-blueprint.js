2ccf4ffcf93ab2e62c5ac30d068e70638c0f5ccf
Tina Steiger <tsteiger@networkedassets.org>
2015-07-21 17:24:20 +0200

    Added more chapters

--- src/main/resources/js/space-blueprint.js
+++ src/main/resources/js/space-blueprint.js
@@ -2,17 +2,17 @@
   and a suffix of " Home Page".*/
 
 AJS.bind("blueprint.wizard-register.ready", function () {
-    function submitExampleSpace(e, state) {
+    function submitArcSpace(e, state) {
         state.pageData.ContentPageTitle = state.pageData.name + " Home Page"; 
         return Confluence.SpaceBlueprint.CommonWizardBindings.submit(e, state);
     }
-    function preRenderExampleSpace(e, state) {
+    function preRenderArcSpace(e, state) {
         state.soyRenderContext['atlToken'] = AJS.Meta.get('atl-token');
         state.soyRenderContext['showSpacePermission'] = false; 
     }
    Confluence.Blueprint.setWizard('com.networkedassets.plugins.space-blueprint:space-blueprint-item', function(wizard) {
-        wizard.on("submit.exampleSpaceId", submitExampleSpace);
-        wizard.on("pre-render.exampleSpaceId", preRenderExampleSpace);
+        wizard.on("submit.exampleSpaceId", submitArcSpace);
+        wizard.on("pre-render.exampleSpaceId", preRenderArcSpace);
         wizard.on("post-render.exampleSpaceId", Confluence.SpaceBlueprint.CommonWizardBindings.postRender);
     });
 });
