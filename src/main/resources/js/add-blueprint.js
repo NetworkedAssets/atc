e70e34694d3ab586b2de205df3cb215dd178d40b
Mikołaj Robakowski <mrobakowski@networkedassets.org>
2015-11-05 13:44:45 +0100

    Switch to using the old, deprecated aui dialog1. Confluence @ Kabel doesn't dupport dialog2.

--- src/main/resources/js/add-blueprint.js
+++ src/main/resources/js/add-blueprint.js
@@ -4,19 +4,58 @@
 
 AJS.toInit(function () {
     console.log("stuffff!!!!");
-    AJS.$("#add-arc42-existing-space-link-id").click(function() {
-        AJS.dialog2("#add-arc42-dialog").show();
+
+    var dialog = new AJS.Dialog({
+        width: 600,
+        height: 300,
+        id: "example-dialog",
+        closeOnOutsideClick: true
     });
 
-    AJS.$("#add-arc42-close-button").click(function(e) {
-        e.preventDefault();
-        AJS.dialog2("#add-arc42-dialog").hide();
-    });
+    dialog.addHeader("Add ARC42 blueprint");
+    dialog.addPanel("DefaultPanel", '\
+        <form action="/plugins/space-blueprint/add-arc42-existing.action" method="get" id="add-arc42-dialog-form" class="aui">\
+            <fieldset class="group">\
+            <legend><span>Blueprint options</span></legend>\
+            <div class="checkbox">\
+                <input class="checkbox" type="checkbox" name="isLabeled" id="arc42isLabeled" checked="checked">\
+                <label for="arc42isLabeled">Add "arc42-added" labels to inserted pages</label>\
+            </div>\
+            <div class="checkbox">\
+                <input class="checkbox" type="checkbox" name="overwrite" id="arc42overwrite">\
+                <label for="arc42overwrite">Overwrite current page instead of adding as a child</label>\
+            </div>\
+        \
+            <input id="arc42spaceKey" type="hidden" value="foo" name="key" />\
+            <input id="arc42pageId" type="hidden" value="-1" name="pageId" />\
+            </fieldset>\
+        </form>', "");
 
-    AJS.$("#add-arc42-add-button").click(function(e) {
-        e.preventDefault();
+    dialog.addButton("Add", function() {
         AJS.$("#arc42spaceKey").val(AJS.Data.get("space-key"));
         AJS.$("#arc42pageId").val(Confluence.getContentId());
         document.getElementById("add-arc42-dialog-form").submit();
-    })
+    });
+
+    dialog.addLink("Close", function() {
+        dialog.hide();
+    });
+
+    AJS.$("#add-arc42-existing-space-link-id").click(function () {
+        dialog.show();
+    });
+
+    ////////////////////////////////////////////////////////
+
+    //AJS.$("#add-arc42-close-button").click(function (e) {
+    //    e.preventDefault();
+    //    AJS.dialog2("#add-arc42-dialog").hide();
+    //});
+    //
+    //AJS.$("#add-arc42-add-button").click(function (e) {
+    //    e.preventDefault();
+    //    AJS.$("#arc42spaceKey").val(AJS.Data.get("space-key"));
+    //    AJS.$("#arc42pageId").val(Confluence.getContentId());
+    //    document.getElementById("add-arc42-dialog-form").submit();
+    //})
 });
